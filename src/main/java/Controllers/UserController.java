7845ec08433bf76180779d10fe290a2b05baaee0
Veres Gábor <Veres.Gabor@newline.hu>
2022-02-08 11:15:50 +0100

    fixed endpoint issue

--- src/main/java/Controllers/UserController.java
+++ src/main/java/Controllers/UserController.java
@@ -165,7 +165,7 @@ public class UserController {
         LocalDateTime today = LocalDateTime.now().minusHours(1);
         QuestionnaireDTO response = new QuestionnaireDTO();
         try {
-            Questionnaire questionnaire = questionnaireRepository.getQuestionnaireById(3L);
+            Questionnaire questionnaire = questionnaireRepository.getQuestionnaireById(3l);
             questionnaire.setDeadline(today);
             questionnaireRepository.save(questionnaire);
             response.setSuccessful(true);
